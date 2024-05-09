c91b915fd65adc62dc7c59ee34bfac65dd82c72f
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-09 20:22:25 +0530

    Update LengthOftheLastWord.java

--- LengthOftheLastWord.java
+++ LengthOftheLastWord.java
@@ -1,28 +1,46 @@
-58. Length of Last Word
-Solved
-Easy
-Topics
-Companies
-Given a string s consisting of words and spaces, return the length of the last word in the string.
+// 58. Length of Last Word
+// Solved
+// Easy
+// Topics
+// Companies
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
 
-A word is a maximal 
-substring
- consisting of non-space characters only.
+// A word is a maximal 
+// substring
+//  consisting of non-space characters only.
 
  
 
-Example 1:
+// Example 1:
 
-Input: s = "Hello World"
-Output: 5
-Explanation: The last word is "World" with length 5.
-Example 2:
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
 
-Input: s = "   fly me   to   the moon  "
-Output: 4
-Explanation: The last word is "moon" with length 4.
-Example 3:
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
 
-Input: s = "luffy is still joyboy"
-Output: 6
-Explanation: The last word is "joyboy" with length 6.
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String str = s.trim();
+        int len = str.length();
+        int count = 0;
+        for(int i=len-1; i>=0; i--)
+        {
+            char ch = str.charAt(i);
+            if(ch==' ')
+            {
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+}
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
