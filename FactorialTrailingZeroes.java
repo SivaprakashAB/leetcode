77bcb589d1c8e0ec5370888e89dfa0b0cb1dc9f3
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-10 18:47:45 +0530

    Update FactorialTrailingZeroes.java

--- FactorialTrailingZeroes.java
+++ FactorialTrailingZeroes.java
@@ -1,23 +1,36 @@
-172. Factorial Trailing Zeroes
-Solved
-Medium
-Topics
-Companies
-Given an integer n, return the number of trailing zeroes in n!.
+// 172. Factorial Trailing Zeroes
+// Solved
+// Medium
+// Topics
+// Companies
+// Given an integer n, return the number of trailing zeroes in n!.
 
-Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+// Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
 
  
 
-Example 1:
+// Example 1:
 
-Input: n = 3
-Output: 0
-Explanation: 3! = 6, no trailing zero.
-Example 2:
+// Input: n = 3
+// Output: 0
+// Explanation: 3! = 6, no trailing zero.
+// Example 2:
 
-Input: n = 5
-Output: 1
+// Input: n = 5
+// Output: 1
+
+
+ class Solution {
+    public int trailingZeroes(int n) {
+      int result =0;
+      while(n>0)
+      {
+        n = n/5;
+        result += n;
+      }
+      return result;
+    }
+}
 Explanation: 5! = 120, one trailing zero.
 Example 3:
 
