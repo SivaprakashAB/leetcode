cb09f597e6cf9b113f0662bf21c25f346034baf0
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-10 18:36:11 +0530

    Update IsSubsequence.java

--- IsSubsequence.java
+++ IsSubsequence.java
@@ -1,22 +1,22 @@
-392. Is Subsequence
-Solved
-Easy
-Topics
-Companies
-Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+// 392. Is Subsequence
+// Solved
+// Easy
+// Topics
+// Companies
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
  
 
-Example 1:
+// Example 1:
 
-Input: s = "abc", t = "ahbgdc"
-Output: true
-Example 2:
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
 
-Input: s = "axc", t = "ahbgdc"
-Output: false
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
 
 class Solution {
     public boolean isSubsequence(String s, String t) {
