d4ef61d1e15174952d9bc6fb266ba80f4d9b5f82
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-16 19:04:17 +0530

    Update AddTwoNumbersUsingLinkedList.java

--- AddTwoNumbersUsingLinkedList.java
+++ AddTwoNumbersUsingLinkedList.java
@@ -37,24 +37,24 @@
 //  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-      ListNode newnode = new ListNode(0);
-      ListNode head = newnode;
+      ListNode newnode = new ListNode(0);  // create a newnode
+      ListNode head = newnode;  //to assign the newnode base address to another listnode
       int bal=0;
-      while( l1 != null || l2 != null)
+      while( l1 != null || l2 != null)  // to check the l1 and l2 is not null
       {
-        int sum = 0+bal;
+        int sum = 0+bal;  // every loop to add the sum
          if(l1!=null)
          {
-            sum = sum+l1.val;
-            l1 = l1.next;
+            sum = sum+l1.val;  // to add the sum addition to the l1 value
+            l1 = l1.next;  // to trace the l1 value
          }
          if(l2!=null)
          {
-            sum = sum+l2.val;
-            l2 = l2.next;
+            sum = sum+l2.val;  // to add the sum addition to the l2 value
+            l2 = l2.next;  // to trace the l2 value
          }
-         bal = sum/10;
-         sum = sum%10;
+         bal = sum/10;  //8+2 = 10 then bal = 1  to add this bal to next sum value
+         sum = sum%10; // 8+2 = 10 the sum = 0;
          head.next = new ListNode(sum);
          head = head.next;
     }
