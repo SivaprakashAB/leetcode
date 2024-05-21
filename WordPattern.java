a865fd0d9a38fef760608fa8c7b373d0e8a4fb58
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-21 12:53:48 +0530

    Update WordPattern.java

--- WordPattern.java
+++ WordPattern.java
@@ -22,3 +22,40 @@
 
 // Input: pattern = "aaaa", s = "dog cat cat dog"
 // Output: false
+
+
+class Solution {                                                                 //this problem to solve with the help of the HashMap
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> char_Map = new HashMap<>();                        //to create two Hashmap one is (Character , String)  another one is (String , Character)
+        Map<String , Character> word_Map = new HashMap<>();
+        String str[] = s.split(" ");                                              // to split the string where is present space (" ") based
+        if(pattern.length() != str.length){                                       // if the both string length is differ then return false
+            return false;
+        }
+        for(int i=0;i<str.length;i++)
+        {
+                char ch = pattern.charAt(i);                                     // to get the every character from the pattern string
+                String word = str[i];                                            // to get the first word from the string
+                if(!char_Map.containsKey(ch))                                    // check if the character (Key) already not contains
+                {
+                    if(char_Map.containsValue(word))                              //the value is already present then return false
+                    {
+                        return false;
+                    }
+                    else                                                          // the value is not present then add with the map with key , Value
+                    {
+                        char_Map.put(ch,word);
+                    }
+                }
+                else                                                          //if the key already present then to get the key and to compare with the value of the current 
+                {
+                    String Mapped = char_Map.get(ch);
+                    if(!Mapped.equals(word))                                  //if it is equals then forget else return false
+                    {
+                        return false;
+                    }
+                }
+        }
+        return true;                                                      //if the iterate fully then successfully wordpattern is eqaul to another then return true
+    }
+}
