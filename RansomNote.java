64980bb248b157d9c4721f7d3f86787681b30aa6
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-21 12:37:05 +0530

    Update RansomNote.java

--- RansomNote.java
+++ RansomNote.java
@@ -22,3 +22,18 @@
 
 // Input: ransomNote = "aa", magazine = "aab"
 // Output: true
+
+class Solution {                                                             //  this question used to solve the hashing method 
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] ans = new int[26];                   // to create a integer array which is have element equal to alphabet length
+        for(char ch : magazine.toCharArray()){     // to change the magazine string convert char array and to iterate with for each method
+            ans[ch-'a']++;                         // to increase the suitable element address value in the ans array
+        }
+        for(char ch: ransomNote.toCharArray()){
+            ans[ch-'a']--;                         // to decrease the suitable element address value in the ans array
+            if(ans[ch-'a']<0)                      // if does any have the value in the ans array then return false ( difference to ransomenote , magazine )
+              return false;
+        }
+        return true;                               //if all itertor is finish then return true
+    }
+}
