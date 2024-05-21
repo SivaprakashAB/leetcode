7279ef665c362e21dd87f625efb58e57d54cc0a3
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-21 12:44:54 +0530

    Update IsomorphicStrings.java

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -24,4 +24,37 @@
 
 // Input: s = "paper", t = "title"
 // Output: true
+
+
+public class Solution {
+
+  boolean isIsomorphic(String s, String t) {
+
+    if (s.length() != t.length()) {             // Check the length of the both if it is not same then return false
+      return false;
+    }
+
+    Map<Character, Character> charMappingMap = new HashMap<>();    // Create a hashmap to store character mappings (this problem solving iwht HashMap)
+
+    for (int i = 0; i < s.length(); i++) {
+
+      char original = s.charAt(i);                                   // to get the first character of the s string
+      char replacement = t.charAt(i);                                // to get the first character of the t string
+
+      if (!charMappingMap.containsKey(original)) {                   //to check if the original is already contains the HashMap (Key)
+        if (!charMappingMap.containsValue(replacement))              //to check if the replacement is already contains the HashMap (Value)
+          charMappingMap.put(original, replacement);                 //then put the key and value in the map
+        else
+          return false;                                              // if the key does not contain but the value is contain then definitely is not Isomorphic 
+      }
+      else {
+        char mappedCharacter = charMappingMap.get(original);         // if the key contains already
+        if (mappedCharacter != replacement)                          // check the key and value is equal if it not equal then return false;
+          return false;
+      }
+    }
+    return true;                                                     // if the iterate successfully then the two string are Isomorphic so  return true
+  }
+
+}
  
