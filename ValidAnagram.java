f8d5f3c99ade22bcfade36280eb691eb4b7dc61b
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-22 22:18:19 +0530

    Update ValidAnagram.java

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -22,24 +22,24 @@
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        String str1 = s.toLowerCase();
-        String str2 = t.toLowerCase();
+        String str1 = s.toLowerCase();      // to generate the lowerecase of the first string
+        String str2 = t.toLowerCase();      // to generte the lowercase of the second string
 
-        str1 = str1.replace(" ","");
+        str1 = str1.replace(" ","");    //to delete the spaces
         str2 = str2.replace(" ","");
 
-        int[] count = new int[26];
+        int[] count = new int[26];   //initrialize the array which equal to the alphabet length
 
         for(int i=0;i<str1.length(); i++){
-            count[str1.charAt(i)-'a']++;
+            count[str1.charAt(i)-'a']++;           //to increase the count when a alphaber is present using ascii value
         }
         for(int i=0;i<str2.length(); i++){
-            count[str2.charAt(i)-'a']--;
+            count[str2.charAt(i)-'a']--;         //to decrease the count when a alphabet is present which present in the first string
         }
         for(int ch : count){
             if(ch!=0)
-               return false;
+               return false;        // if the ch is not zero then return false
         }
-        return true;
+        return true;               // if the iteration is end wihtout any distract then return true
     }
 }
