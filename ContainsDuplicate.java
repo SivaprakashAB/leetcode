d2caba5f1e9c3949d3b4552d8762d695343ec9ec
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-22 22:27:20 +0530

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -19,4 +19,17 @@
 
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       HashSet<Integer> ans= new HashSet<>();              //Using hashset to check duplicate
+       for(int i=0;i<nums.length;i++){
+        if(ans.contains(nums[i])){                        //check the number is come which also present in the previously than it is duplicate to return true
+            return true;
+        }
+        ans.add(nums[i]);                                // if the number does not come previously than to add the hashset certainly
+       }
+       return false;                                  // if the loop is complete without any distract than return false
+    }
+}
  
