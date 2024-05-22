1c8fdf013dcbf6c518ed3cc0e99bac13b43149b3
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-22 22:15:15 +0530

    Update ValidAnagram.java

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -18,3 +18,28 @@
 
 // Input: s = "rat", t = "car"
 // Output: false
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        String str1 = s.toLowerCase();
+        String str2 = t.toLowerCase();
+
+        str1 = str1.replace(" ","");
+        str2 = str2.replace(" ","");
+
+        int[] count = new int[26];
+
+        for(int i=0;i<str1.length(); i++){
+            count[str1.charAt(i)-'a']++;
+        }
+        for(int i=0;i<str2.length(); i++){
+            count[str2.charAt(i)-'a']--;
+        }
+        for(int ch : count){
+            if(ch!=0)
+               return false;
+        }
+        return true;
+    }
+}
