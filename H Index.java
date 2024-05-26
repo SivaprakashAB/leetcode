1047ce919113dc6ae8b7c39c5a8637e2ae0d1397
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2024-05-26 15:17:17 +0530

    Update H Index.java

--- H Index.java
+++ H Index.java
@@ -27,3 +27,84 @@
 // n == citations.length
 // 1 <= n <= 5000
 // 0 <= citations[i] <= 1000
+
+SOLUTION - 1
+ 
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);             // to sort the array 
+        int i=0;
+        while( i < citations.length && citations [citations.length-1-i] > i)  // to check the i value is less than citations length and check back to front value is grearter than i
+               i++;
+        return i;
+    }
+}
+
+SOLUTION - 2
+ 
+ class Solution {
+    public int hIndex(int[] citations) {
+        int l = citations.length;
+        int[] buckets = new int[l+1];
+        
+        for (int c:citations){
+            if (c>=l){
+                buckets[l]++;
+            }else{
+                buckets[c]++;
+            }
+        }
+
+        int count = 0;
+        for (int i=l; i>=0; i--){
+            count+=buckets[i];
+            if (count>=i){
+                return i;
+            }
+        }
+         return 0;
+    }
+}
+
+SOLUTION - 3
+
+ class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        sort(citations);
+        
+        int count = 0;
+        int ans = 0;
+        for(int i=n-1;i>=0;i--){
+            count++;
+            if(citations[i] >= count){
+                ans = Math.max(count,ans);
+            }
+            else
+            break;
+        }
+    
+    return ans;
+    }
+
+    void sort(int[] nums){
+        int max = -1;
+        for(int x : nums){
+            if(x>max)
+            max = x;
+        }
+
+        int[] arr = new int[max+1];
+
+        for(int x : nums)
+        arr[x]++;
+
+        int p = 0;
+        for(int i=0;i<=max;i++){
+            while(arr[i]!=0){
+            nums[p++] = i;
+            arr[i]--;
+            }
+        }
+
+    }
